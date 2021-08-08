28138ea16225122cf5ea0d2be1fd6d2d67641f86
Murali Krishna <muralikrishna@Muralis-MacBook-Air.local>
2021-08-08 18:11:11 +0530

    development process

--- test.py
+++ test.py
@@ -1,2 +1,38 @@
 def printMarks():
 	print("printMarks")
+
+dev -> test -> prod -> demo
+
+dev - develop -> deploy -> filesystem -> dev finish
+
+
+dev -> changes -> app/test -> changes push -> reviewer -> review -> changes -> doing changes -> push -> review -> changes approve -> changes merge to test branch
+
+
+
+app -> master
+	-> test --> bugs -> resolve -> changes -> review process -> approve merge-> ok -> merge with master -> prod
+	-> dev -> not necessary
+
+
+
+dev => dev app dev.com -> filesystem
+
+test => test app test.com -> database -> each file -> data -> record
+
+
+filename validfrom validto filedata
+
+test 6:05 6:07 file_data
+
+test 6:07 9999-12-31 file_data
+
+
+
+master -> prod => prod.com ->fs
+
+
+
+
+
+
